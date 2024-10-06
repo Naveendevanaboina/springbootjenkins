27dac21791acb2b187ca6b3d9d476423dfb0614f
Naveendevanaboina <devanaboinanaveen2@gmail.com>
2024-10-06 10:42:38 +0530

    jenkines

---
+++ src/main/java/com/neoteric/springdocker2/springdocker2/Springdocker2Application.java
@@ -0,0 +1,13 @@
+package com.neoteric.springdocker2.springdocker2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springdocker2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springdocker2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/neoteric/springdocker2/springdocker2/Springdocker2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neoteric.springdocker2.springdocker2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springdocker2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
